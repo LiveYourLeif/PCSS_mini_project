86664085e23e0bb46df3b0f163561b41f44c1087
Sompoon <alind19@student.aau.dk>
2021-09-21 12:51:49 +0200

    Playerclass workings

---
+++ ClassStats.py
@@ -0,0 +1,56 @@
+import random
+import Stats
+
+
+class Warrior:
+    hpWarrior = Stats.HP()
+    hpWarrior.set_HP(50)
+    print(f"HP: {hpWarrior.get_HP()}")
+
+    strengthWarrior = Stats.Strength()
+    strengthWarrior.set_strength(random.randint(6, 8))
+    print(f"Strength: {strengthWarrior.get_strength()}")
+
+    defenceWarrior = Stats.Defence()
+    defenceWarrior.set_defence(random.randint(8,10))
+    print(f"Defence: {defenceWarrior.get_defence()}")
+
+    magicWarrior = Stats.Magic()
+    magicWarrior.set_magic(0)
+    print(f"Magic: {magicWarrior.get_magic()}")
+
+
+class Mage:
+    hpMage = Stats.HP()
+    hpMage.set_HP(50)
+    print(f"HP: {hpMage.get_HP()}")
+
+    strengthMage = Stats.Strength()
+    strengthMage.set_strength(random.randint(1, 3))
+    print(f"Strength: {strengthMage.get_strength()}")
+
+    defenceMage = Stats.Defence()
+    defenceMage.set_defence(random.randint(1, 3))
+    print(f"Defence: {defenceMage.get_defence()}")
+
+    magicMage = Stats.Magic()
+    magicMage.set_magic(random.randint(8, 10))
+    print(f"Magic: {magicMage.get_magic()}")
+
+class Wildcard:
+    hpWildCard = Stats.HP()
+    hpWildCard.set_HP(50)
+    print(f"HP: {hpWildCard.get_HP()}")
+
+    strengthWildCard = Stats.Strength()
+    strengthWildCard.set_strength(random.randint(0,10))
+    print(f"Strength: {strengthWildCard.get_strength()}")
+
+    defenceWildCard = Stats.Defence()
+    defenceWildCard.set_defence(random.randint(0,10))
+    print(f"Defence: {defenceWildCard.get_defence()}")
+
+    magicWildCard = Stats.Magic()
+    magicWildCard.set_magic(random.randint(0, 10))
+    print(f"Magic: {magicWildCard.get_magic()}")
+
--- PlayerClass.py
+++ PlayerClass.py
@@ -1,23 +1,28 @@
-class Player:
-    playerName = input("Choose your name wanderer! ")
+from Stats import Stats as Stats #Stats
+
+
+class Player(Stats):
+    playerName = input("Choose your name, wanderer! ")
     print(f"Hello {playerName}!")
 
-    print (f"Which class do you want to be? \n1 = Warrior. 2 = Mage. 3 = Wildcard"
+    print(f"Which class do you want to be? \n1 = Warrior. 2 = Mage. 3 = Wildcard"
                         f"\nThe Warrior has high defense and a powerful attack, but is very weak to magic"
                         f"\nThe Mage has overly superior magic powers, but is weak to regular hits from enemies"
                         f"\nThe Wild Card has random stats! Roll the dice and prepare your numpys")
     playerClass = input(f"Choose your class, with either the number 1, 2 or 3")
 
-    playerChoice = int(playerClass) # Secures the input from the user when choosing their class
+    while 1:
+        if playerChoice == 1:
+            print(f"\nYou have chosen the warrior, great choice {playerName}, get ready for an exciting adventure!")
+            break
+        elif playerChoice == 2:
+            print(f"\nYou have chosen the mage, great choice {playerName}, get ready for an exciting adventure!")
+            break
+        elif playerChoice == 3:
+            print(f"\nYou have chosen the wild card, brave choice {playerName}, get ready for an exciting adventure!")
+            break
+        else:
+            print(f"Your choice was not a class, {playerName}")
+            playerChoice = int(input(f"Choose your class, with either the number 1, 2 or 3 : "))
 
-    if playerChoice > 3 or playerChoice < 1:
-       print(f"Your choice was not a class {playerName}")
-       playerClass = input(f"Choose again, a number between 1, 2 or 3")
-
-    if playerChoice == 1:
-        print(f"\nYou have chosen the warrior, great choice {playerName}, get ready for an exciting adventure!")
-    if playerChoice == 2:
-        print(f"\nYou have chosen the mage, great choice {playerName}, get ready for an exciting adventure!")
-    if playerChoice == 3:
-        print(f"\nYou have chosen the wild card, brave choice {playerName}, get ready for an exciting adventure!")
 
