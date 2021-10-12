58a5b4304e0e11566ab1851a1f96e1965df8e2bd
Sompoon <alind19@student.aau.dk>
2021-10-12 13:51:22 +0200

    Tilføjet samtlige \n for at konsolen er mere læselig

--- Encounters.py
+++ Encounters.py
@@ -6,7 +6,6 @@ import PlayerClass
 class Encounter():
 
     def story(self):
-        print("")
         print("Your task is to defeat the legendary enemy who is threatening the kingdom.\n"
               "You wander out into the wilderness. ")
 
@@ -29,26 +28,26 @@ class Encounter():
         while enemyHP > 0:
             playerAction = int(input(f"It is your turn! What do you want to do?\n"
                                      f"Press 1 for a melee attack.\n"
-                                     f"Press 2 for a magic attack."))
+                                     f"Press 2 for a magic attack. \n"))
             if playerAction == 1:
                 if PlayerClass.Player.playerChoice == 1:
                     currentStrength = PlayerClass.Player.warrior.classStrength() # Varible to set the current strength of the enemy
                     print(f"Enemy health: {currentEnemyHP}")
                     print(f"You swing at the enemy with your sword!")
                     print(f"You dealt {currentStrength} damage to the {enemyName}")
-                    print(f"{enemyName} health: {currentEnemyHP - currentStrength}")
+                    print(f"{enemyName} health: {currentEnemyHP - currentStrength} \n")
 
                 if PlayerClass.Player.playerChoice == 2:
                     currentStrength = PlayerClass.Player.mage.classStrength()
                     print(f"You slap the enemy with the back of your hand!")
                     print(f"You dealt {currentStrength} damage to the {enemyName}")
-                    print(f"{enemyName} health: {enemyHP - currentStrength}")
+                    print(f"{enemyName} health: {enemyHP - currentStrength} \n")
 
                 if PlayerClass.Player.playerChoice == 3:
                     currentStrength = PlayerClass.Player.wildcard.classStrength()
                     print(f"You roundhouse kick the enemy in the throat!")
                     print(f"You dealt {PlayerClass.Player.wildcard.classStrength()} damage to the {enemyName}")
-                    print(f"{enemyName} health: {enemyHP - currentStrength}")
+                    print(f"{enemyName} health: {enemyHP - currentStrength} \n")
 
 
             if playerAction == 2:
@@ -56,19 +55,19 @@ class Encounter():
                     currentMagic = PlayerClass.Player.warrior.classMagic()
                     print(f"You try to cast a fireball, but only tickle the enemy with sparks!")
                     print(f"You dealt {currentMagic} damage to the {enemyName}")
-                    print(f"{enemyName} health: {enemyHP - currentMagic}")
+                    print(f"{enemyName} health: {enemyHP - currentMagic} \n")
 
                 if PlayerClass.Player.playerChoice == 2:
                     currentMagic = PlayerClass.Player.mage.classMagic()
                     print(f"You use a powerful magic attack on the enemy!")
                     print(f"You dealt {currentMagic} damage to the {enemyName}")
-                    print(f"{enemyName} health: {enemyHP - currentMagic}")
+                    print(f"{enemyName} health: {enemyHP - currentMagic} \n")
 
                 if PlayerClass.Player.playerChoice == 3:
                     currentMagic = PlayerClass.Player.wildcard.classMagic()
                     print(f"You cast a magic spell on the enemy!")
                     print(f"You dealt {PlayerClass.Player.wildcard.classMagic()} damage to the {enemyName}")
-                    print(f"{enemyName} health: {enemyHP - currentMagic}")
+                    print(f"{enemyName} health: {enemyHP - currentMagic} \n")
 
             if enemyHP <= 0:
                 print("You killed the enemy, good job, mom get the camera")
