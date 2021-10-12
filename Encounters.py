16aeed3972b4faf2ee10c08b918ef0bb007ceb6d
kristian1402 <ksaren20@student.aau.dk>
2021-10-12 13:01:51 +0200

    all classes can deal damage once

--- Encounters.py
+++ Encounters.py
@@ -20,29 +20,32 @@ class Encounter():
         newEnemy = enemylist.pop(0)
 
         # Print the name of the new enemy
-        firstEnemyName = newEnemy.className()
-        firstEnemyHP = newEnemy.classHP()
-        print(f"A {firstEnemyName} approaches!")
+        EnemyName = newEnemy.className()
+        EnemyHP = newEnemy.classHP()
+        print(f"A {EnemyName} approaches!")
 
-
-        while firstEnemyHP > 0:
+        # Battle between the player and the enemy mob
+        while EnemyHP > 0:
             playerAction = int(input(f"It is your turn! What do you want to do?"))
             if playerAction == 1:
                 if PlayerClass.Player.playerChoice == 1:
+                    currentStrength = PlayerClass.Player.warrior.classStrength() # Varible to set the current strength of the enemy
                     print(f"You swing at the enemy with your sword!")
-                    print(f"You dealt {PlayerClass.Player.warrior.classStrength()} damage to the {firstEnemyName}")
-                    print(f"{firstEnemyName} health: {firstEnemyHP - PlayerClass.Player.warrior.classStrength()}")
-                    break
+                    print(f"You dealt {currentStrength} damage to the {EnemyName}")
+                    print(f"{EnemyName} health: {EnemyHP - currentStrength}")
+
                 if PlayerClass.Player.playerChoice == 2:
+                    currentStrength = PlayerClass.Player.mage.classStrength()
                     print(f"You slap the enemy with the back of your hand!")
-                    print(f"You dealt {PlayerClass.Player.mage.classStrength()} damage to the {firstEnemyName}")
-                    print(f"{firstEnemyName} health: {firstEnemyHP - PlayerClass.Player.mage.classStrength()}")
-                    break
+                    print(f"You dealt {currentStrength} damage to the {EnemyName}")
+                    print(f"{EnemyName} health: {EnemyHP - currentStrength}")
+
                 if PlayerClass.Player.playerChoice == 3:
+                    currentStrength = PlayerClass.Player.wildcard.classStrength()
                     print(f"You roundhouse kick the enemy in the throat!")
-                    print(f"You dealt {PlayerClass.Player.wildcard.classStrength()} damage to the {firstEnemyName}")
-                    print(f"{firstEnemyName} health: {firstEnemyHP - PlayerClass.Player.wildcard.classStrength()}")
-                    break
+                    print(f"You dealt {PlayerClass.Player.wildcard.classStrength()} damage to the {EnemyName}")
+                    print(f"{EnemyName} health: {EnemyHP - currentStrength}")
+
 
             #if PlayerClass.Player.playerChoice == 2:
                 #if Playerclass.Player.player
